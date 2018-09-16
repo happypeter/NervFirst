ae8182970ee5608e752bb35f2de36dab496655d2
Peter Wang <happypeter1983@gmail.com>
2018-09-16 17:05:24 +0800

    interact

---
+++ interact/index.js
@@ -0,0 +1,31 @@
+const nervos = require('./nervos')
+const simpleStore = require('./simpleStore')
+const text = `hello ${new Date()}`
+const time = new Date()
+;(async function() {
+  const current = await nervos.appchain.getBlockNumber()
+  const tx = {
+    ...simpleStore.transaction,
+    validUntilBlock: +current + 88
+  }
+  const res = await simpleStore.simpleStoreContract.methods
+    .add(text, +time)
+    .send(tx)
+
+  const receipt = await nervos.listeners.listenToTransactionReceipt(res.hash)
+  receipt.errorMessage && console.log(receipt.errorMessage)
+  const from =
+    nervos.appchain.accounts.wallet[0] &&
+    nervos.appchain.accounts.wallet[0].address
+
+  const times = await simpleStore.simpleStoreContract.methods.getList().call({
+    from
+  })
+  console.log('times', times)
+  const messages = await Promise.all(
+    times.map(time =>
+      simpleStore.simpleStoreContract.methods.get(time).call({ from })
+    )
+  )
+  console.log('messages', messages)
+})()
